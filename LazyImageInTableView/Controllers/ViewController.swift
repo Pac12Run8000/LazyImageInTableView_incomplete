880ac219ba9f49a1ff5f3989f75e289b04da04dc
Pac12Run8000 <gangstarr78992@>
2022-11-18 10:45:37 -0800

    Project is working

--- LazyImageInTableView/Controllers/ViewController.swift
+++ LazyImageInTableView/Controllers/ViewController.swift
@@ -8,16 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var tblViewArray:[Movie] = [] {
+        didSet {
+            print(tblViewArray)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         NetworkingService.shared.fetchData { movies in
             for item in movies.results {
-                print("********")
-                print("name:",item.title)
-                print("bd:", item.backdropPath.tmdbImagePath)
-                print("pp:", item.backdropPath.tmdbImagePath)
-                print("********")
+                self.tblViewArray.append(Movie(name: item.title, bd: item.backdropPath.tmdbImagePath, pp: item.posterPath.tmdbImagePath))
             }
         }
     }
---
+++ LazyImageInTableView/Models/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  LazyImageInTableView
+//
+//  Created by Michelle Grover on 11/18/22.
+//
+
+import Foundation
+
+struct Movie {
+    var name:String
+    var bd:String
+    var pp:String
+}
