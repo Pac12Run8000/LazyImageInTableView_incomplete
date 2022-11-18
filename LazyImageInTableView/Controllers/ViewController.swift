c4b9d15ad628a7d5972fced66fc61f2e0dcc36ea
Pac12Run8000 <gangstarr78992@>
2022-11-18 11:01:43 -0800

    Working version

--- LazyImageInTableView/Controllers/ViewController.swift
+++ LazyImageInTableView/Controllers/ViewController.swift
@@ -9,14 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    
     var tblViewArray:[Movie] = [] {
         didSet {
-            print(tblViewArray)
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+            
         }
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
         NetworkingService.shared.fetchData { movies in
             for item in movies.results {
                 self.tblViewArray.append(Movie(name: item.title, bd: item.backdropPath.tmdbImagePath, pp: item.posterPath.tmdbImagePath))
@@ -25,5 +34,22 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension ViewController:UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let item = tblViewArray[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = item.name
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tblViewArray.count
+    }
+    
+    
+    
 }
 
