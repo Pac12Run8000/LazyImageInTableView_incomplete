efb1435f927069365e8c4f8bfa025de20a45efa1
Pac12Run8000 <gangstarr78992@>
2022-11-19 04:33:26 -0800

    Working version

--- LazyImageInTableView/CustomObjects/CustomImageView.swift
+++ LazyImageInTableView/CustomObjects/CustomImageView.swift
@@ -7,74 +7,38 @@
 
 import UIKit
 
-var imgCache = NSCache<AnyObject, AnyObject>()
+
 
 class CustomImageView: UIImageView {
     
-    /*
-    var task:URLSessionDataTask?
-    var spinner = UIActivityIndicatorView(style: .medium)
-    */
+    private let imageCache = NSCache<AnyObject, UIImage>()
    
     func loadImage(urlStr:String) {
-        
         DispatchQueue.main.async {
-            self.image = UIImage(systemName: "menucard.fill")
+            self.image = UIImage(systemName: "menubar.dock.rectangle.badge.record")
+            
+            
             guard let url = URL(string: urlStr) else {fatalError("No url")}
+            
+            if let cachedImage = self.imageCache.object(forKey: url as AnyObject) {
+                print("Images loaded from cache")
+                self.image = cachedImage
+                return
+            }
+            
             DispatchQueue.global(qos: .userInteractive).async {
                 guard let data = try? Data(contentsOf: url) else {fatalError("No data")}
                 guard let image = UIImage(data: data) else {fatalError("No image")}
+                guard let resizedImage = image.resizeImageTo(size: CGSize(width: 80.00, height: 80.00)) else {fatalError("Couldn't resize image")}
+                    print("Images loaded from internet")
                     DispatchQueue.main.async {
-                        self.image = image
+                        self.imageCache.setObject(resizedImage, forKey: url as AnyObject)
+                        self.image = resizedImage
                     }
                 }
         }
     }
     
-/*
-    func loadImage(urlStr:String) {
-        guard let url = URL(string: urlStr) else {return}
-        DispatchQueue.main.async {
-            self.image = nil
-            self.addSpinner()
-            if let task = self.task {
-                task.cancel()
-            }
-            
-            if let imageFromCache = imgCache.object(forKey: url.absoluteString as AnyObject) as? UIImage {
-                self.image = imageFromCache
-                self.spinner.removeFromSuperview()
-                return
-            }
-            
-            self.task = URLSession.shared.dataTask(with: url) { data, response, error in
-                guard let data = data, let newImage = UIImage(data: data) else {
-                    print("error:")
-                    return
-                }
-                imgCache.setObject(newImage, forKey: url.absoluteString as AnyObject)
 
-                DispatchQueue.main.async {
-                    self.image = newImage
-                    self.spinner.removeFromSuperview()
-                }
-
-            }
-            self.task!.resume()
-        }
- 
-    }
-    
- */
-    
-    /*
-    func addSpinner() {
-        addSubview(spinner)
-        spinner.translatesAutoresizingMaskIntoConstraints = true
-        spinner.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
-        spinner.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
-        spinner.startAnimating()
-    }
-     */
 
 }
